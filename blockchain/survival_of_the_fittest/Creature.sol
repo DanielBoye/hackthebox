57c971d1f83de71b76e5c4e185ac1f3a7c7c1bfb
DanielBoye <danielboye888@gmail.com>
2023-09-02 23:02:32 +0200

    Task files

---
+++ blockchain/survival_of_the_fittest/Creature.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Creature {
+    
+    uint256 public lifePoints;
+    address public aggro;
+
+    constructor() payable {
+        lifePoints = 20;
+    }
+
+    function strongAttack(uint256 _damage) external{
+        _dealDamage(_damage);
+    }
+    
+    function punch() external {
+        _dealDamage(1);
+    }
+
+    function loot() external {
+        require(lifePoints == 0, "Creature is still alive!");
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function _dealDamage(uint256 _damage) internal {
+        aggro = msg.sender;
+        lifePoints -= _damage;
+    }
+}
